5a790b34293657d8f342fe11b60947cc61ea3141
Dan Possehl <danielbpossehl@gmail.com>
2021-08-07 11:12:27 -0600

    was able to recode my rows in a way that makes more sense, still need to figure out the CSS issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var myHours = [
     hour: '09',
     time: '09',
     AmPm: 'am',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -12,7 +12,7 @@ var myHours = [
     hour: '10',
     time: '10',
     AmPm: 'am',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -20,7 +20,7 @@ var myHours = [
     hour: '11',
     time: '11',
     AmPm: 'am',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -28,7 +28,7 @@ var myHours = [
     hour: '12',
     time: '12',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -36,7 +36,7 @@ var myHours = [
     hour: '13',
     time: '01',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -44,7 +44,7 @@ var myHours = [
     hour: '14',
     time: '02',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -52,7 +52,7 @@ var myHours = [
     hour: '15',
     time: '03',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -60,7 +60,7 @@ var myHours = [
     hour: '16',
     time: '04',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 
   {
@@ -68,7 +68,7 @@ var myHours = [
     hour: '17',
     time: '05',
     AmPm: 'pm',
-    undefined: '',
+    text: '',
   },
 ]
 
@@ -80,6 +80,24 @@ function getHeaderDate() {
 
 getHeaderDate()
 
+function displayRows() {
+  var container = document.querySelector('.container')
+  for (var i = 0; i < myHours.length; i++) {
+    var form = document.createElement('form')
+    form.setAttribute('class', 'row')
+    var htmlString = `<div class="col-md-2 hour">${myHours[i].time} ${myHours[i].AmPm}</div>
+        <div class="col-md-9 description p-0">
+          <textarea id= ${myHours[i].tag} class="past"></textarea>
+        </div>
+        <button class="col-md-1 saveBtn">
+          <i class="far fa-save fa-lg"></i>
+        </button>`
+    form.innerHTML = htmlString
+    container.appendChild(form)
+  }
+}
+
+displayRows()
 // //Creating rows and columns
 // myHours.forEach(function (currentHour) {
 //   var rows = $('<form>').attr({
@@ -89,45 +107,45 @@ getHeaderDate()
 //   $('.container').append(rows)
 // })
 
-// creates the visuals for the scheduler body
-myHours.forEach(function (thisHour) {
-  // creates timeblocks row
-  var myHours = $('<form>').attr({
-    class: 'row',
-  })
-  $('.container').append(myHours)
+// // creates the visuals for the scheduler body
+// myHours.forEach(function (thisHour) {
+//   // creates timeblocks row
+//   var myHours = $('<form>').attr({
+//     class: 'row',
+//   })
+//   $('.container').append(myHours)
 
-  // creates time field
-  var hourField = $('<div>').text(`${thisHour.hour}${thisHour.meridiem}`).attr({
-    class: 'col-md-2 hour',
-  })
+//   // creates time field
+//   var hourField = $('<div>').text(`${thisHour.hour}${thisHour.meridiem}`).attr({
+//     class: 'col-md-2 hour',
+//   })
 
-  // creates schdeduler data
-  var hourPlan = $('<div>').attr({
-    class: 'col-md-9 description p-0',
-  })
-  var planData = $('<textarea>')
-  hourPlan.append(planData)
-  planData.attr('id', thisHour.id)
-  if (thisHour.time < moment().format('HH')) {
-    planData.attr({
-      class: 'past',
-    })
-  } else if (thisHour.time === moment().format('HH')) {
-    planData.attr({
-      class: 'present',
-    })
-  } else if (thisHour.time > moment().format('HH')) {
-    planData.attr({
-      class: 'future',
-    })
-  }
+//   // creates schdeduler data
+//   var hourPlan = $('<div>').attr({
+//     class: 'col-md-9 description p-0',
+//   })
+//   var planData = $('<textarea>')
+//   hourPlan.append(planData)
+//   planData.attr('id', thisHour.id)
+//   if (thisHour.time < moment().format('HH')) {
+//     planData.attr({
+//       class: 'past',
+//     })
+//   } else if (thisHour.time === moment().format('HH')) {
+//     planData.attr({
+//       class: 'present',
+//     })
+//   } else if (thisHour.time > moment().format('HH')) {
+//     planData.attr({
+//       class: 'future',
+//     })
+//   }
 
-  // creates save button
-  var saveButton = $("<i class='far fa-save fa-lg'></i>")
-  var savePlan = $('<button>').attr({
-    class: 'col-md-1 saveBtn',
-  })
-  savePlan.append(saveButton)
-  myHours.append(hourField, hourPlan, savePlan)
-})
+//   // creates save button
+//   var saveButton = $("<i class='far fa-save fa-lg'></i>")
+//   var savePlan = $('<button>').attr({
+//     class: 'col-md-1 saveBtn',
+//   })
+//   savePlan.append(saveButton)
+//   myHours.append(hourField, hourPlan, savePlan)
+// })
